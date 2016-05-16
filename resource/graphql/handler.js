208c0f668431b8272d9f544e7b2b419f9e2d17c8
Mohd Faiz Hasim <faiz.hasim@servicerocket.com>
2016-05-16 15:53:42 +0800

    ES2015 support with async-await

--- resource/graphql/handler.js
+++ resource/graphql/handler.js
@@ -1,10 +1,19 @@
-'use strict';
+import "babel-polyfill";
+import {test} from '../../../lib/whatever';
 
 var customMsg = "2016-05-12";
 const whatever = () => "asd";
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: `${whatever()} ${customMsg}: Go Serverless! Your Lambda function executed successfully!`
-  });
+const aPromise = () => Promise.resolve('2016-05-16T15:47:13');
+
+const testAsync = async () => {
+  const aPromiseValue = await aPromise();
+  return `val => ${aPromiseValue}`;
 };
+
+export function handler(event, context, cb) {
+
+  return testAsync().then(val => cb(null, {
+    message: `${val} ${test()} ${whatever()} ${customMsg}: Go Serverless! Your Lambda function executed successfully! (Using export function)`
+  }));
+}
